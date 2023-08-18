111d551de0f132c8184922c9c1e1ecd8f67b6669
amandaherbst <amandaeherbst@gmail.com>
2023-08-18 14:27:30 -0700

    Initial commit

---
+++ R/bike_miles.R
@@ -0,0 +1,12 @@
+#' Calculate total miles biked
+#'
+#' @param distance Number of miles biked
+#' @param count Number of times biked
+#'
+#' @return
+#' @export
+#'
+#' @examples
+bike_miles <- function(distance, count) {
+  miles*count
+}
---
+++ R/burritos_cost.R
@@ -0,0 +1,12 @@
+#' Caclulate total burrito cost
+#'
+#' @param price Price per burritos in dollars
+#' @param count Number of burritos in order
+#'
+#' @return
+#' @export
+#'
+#' @examples
+burritos_cost <- function(price, count) {
+  price * count
+}
